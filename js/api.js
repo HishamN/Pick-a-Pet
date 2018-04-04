6b008479f3b6f5320f528b3ad802e5e68df7f369
Claudia Rios <claudiarios@Claudias-MacBook.local>
2018-04-04 19:11:29 -0400

    made changes to ajax calls for nav buttons

--- js/api.js
+++ js/api.js
@@ -54,10 +54,12 @@ $(document).ready(function () {
         renderBtn();
     });
     // grabs the class of gif and returns the user input with the correct gif //
-    $(document).on("click", ".pets-btn", function () {
+    $(document).on("click", "#pets-btn", function (event) {
+        event.preventDefault();
         // remove prior gifs //
-        $('#pets-view').empty();
-        var pets = $(this).attr("data-name");
+        $('.pet-info').empty();
+        var pets = $("#pets-input").val().trim();
+        console.log(pets);
         // var cars = $('#pets-input').val().trim();
         var queryURL = " http://api.petfinder.com/pet.find?&key=af4f1bad7b7c2e0582a6fbfff43c9255&format=json&location=32819&animal=" + pets;
         //Creating an AJax call for the specific pet button being click
@@ -69,6 +71,186 @@ $(document).ready(function () {
             var results = fetch;
             var innerResult = results.petfinder.pets.pet
             console.log(results)
+            
+            for (let i = 0; i < innerResult.length; i++) {
+                const element = innerResult[i];
+                var ourBreeds = [];
+                const breeds = element.breeds.breed;
+                for (let x = 0; x < breeds.length; x++) {
+                    const breed = breeds[x].$t;
+                    ourBreeds.push(breed)
+                    
+                }
+                var petImage = ""
+                if (element.media.photos) { 
+                    petImage = $("<img>").attr("src", element.media.photos.photo[2].$t);
+                
+                    
+                }
+            
+
+                
+                var q = $("<p>").text("Breed: " + ourBreeds);
+                 var p = $("<p>").text("Age: " + element.age.$t);
+                 var r = $("<p>").text("City: " + element.contact.city.$t);
+                 var s = $("<p>").text("Size: " + element.size.$t);
+                 var t = $("<p>").text("Sex: " + element.sex.$t);
+
+                 $(".pet-info").append(petImage)
+                 $(".pet-info").append(q);
+                 $(".pet-info").append(p);
+                 $(".pet-info").append(r);
+                 $(".pet-info").append(s);
+                 $(".pet-info").append(t);
+                 $(".pet-info").append(" ")
+                 
+
+            }
+
+
+
+            console.log(innerResult);
+            //Displaying the rating
+
+            // loop through our results //
+            for (var i = 0; i < results.length; i++) {
+                // assign variable to gif url //
+                var photos = results[i].media.photos.photo;
+                // var still = results[i].images.fixed_height_still.url;
+                //Storing the rating data
+                // var rating = results[i].rating;
+                // console.log("Rating: " + rating);
+
+                // create image //
+                var gifImage = $("<img>");
+                gifImage.addClass("pics");
+                //gifImage.attr('src', still);
+                //gifImage.attr('data-still', still);
+                // gifImage.attr('data-animated', animated);
+
+                // gifImage.attr('data-state', "@size");???????
+                //Retrieving the URL for the image
+                var imgURL = results[i].photos.fixed_height.url;
+                console.log("URL: " + imgURL);
+                //Displaying the rating
+                // var p = $("<p>").text("Rating: " + rating);
+                //Creating element to hold the image
+                var image = $("<img>").attr("src", imgURL);
+                //Appending the image
+                $('#test1').append(gifImage);
+                $('#test1').append(p);
+
+            }
+        });
+
+    });
+    $(document).on("click", ".dog", function (event) {
+        event.preventDefault();
+        // remove prior gifs //
+        $('.pet-info').empty();
+       
+        // var cars = $('#pets-input').val().trim();
+        var queryURL = " http://api.petfinder.com/pet.find?&key=af4f1bad7b7c2e0582a6fbfff43c9255&format=json&location=32819&animal=dog";
+        //Creating an AJax call for the specific pet button being click
+        $.ajax({
+            url: queryURL,
+
+            method: "GET"
+        }).then(function (fetch) {
+            var results = fetch;
+            console.log(results);
+            var innerResult = results.petfinder.pets.pet;
+            // console.log(results)
+            
+            for (let i = 0; i < innerResult.length; i++) {
+                const element = innerResult[i];
+                var ourBreeds = [];
+                const breeds = element.breeds.breed;
+                for (let x = 0; x < breeds.length; x++) {
+                    const breed = breeds[x].$t;
+                    ourBreeds.push(breed)
+                    
+                }
+                var petImage = ""
+                if (element.media.photos) { 
+                    petImage = $("<img>").attr("src", element.media.photos.photo[2].$t);
+                
+                    
+                }
+            
+
+                
+                var q = $("<p>").text("Breed: " + ourBreeds);
+                 var p = $("<p>").text("Age: " + element.age.$t);
+                 var r = $("<p>").text("City: " + element.contact.city.$t);
+                 var s = $("<p>").text("Size: " + element.size.$t);
+                 var t = $("<p>").text("Sex: " + element.sex.$t);
+
+                 $(".pet-info").append(petImage)
+                 $(".pet-info").append(q);
+                 $(".pet-info").append(p);
+                 $(".pet-info").append(r);
+                 $(".pet-info").append(s);
+                 $(".pet-info").append(t);
+                 $(".pet-info").append(" ")
+                 
+
+            }
+
+
+
+            console.log(innerResult);
+            //Displaying the rating
+
+            // loop through our results //
+            for (var i = 0; i < results.length; i++) {
+                // assign variable to gif url //
+                var photos = results[i].media.photos.photo;
+                // var still = results[i].images.fixed_height_still.url;
+                //Storing the rating data
+                // var rating = results[i].rating;
+                // console.log("Rating: " + rating);
+
+                // create image //
+                var gifImage = $("<img>");
+                gifImage.addClass("pics");
+                //gifImage.attr('src', still);
+                //gifImage.attr('data-still', still);
+                // gifImage.attr('data-animated', animated);
+
+                // gifImage.attr('data-state', "@size");???????
+                //Retrieving the URL for the image
+                var imgURL = results[i].photos.fixed_height.url;
+                console.log("URL: " + imgURL);
+                //Displaying the rating
+                // var p = $("<p>").text("Rating: " + rating);
+                //Creating element to hold the image
+                var image = $("<img>").attr("src", imgURL);
+                //Appending the image
+                $('#test1').append(gifImage);
+                $('#test1').append(p);
+
+            }
+        });
+
+    });
+    $(document).on("click", ".cat", function (event) {
+        event.preventDefault();
+        // remove prior gifs //
+        $('.pet-info').empty();
+       
+        // var cars = $('#pets-input').val().trim();
+        var queryURL = " http://api.petfinder.com/pet.find?&key=af4f1bad7b7c2e0582a6fbfff43c9255&format=json&location=32819&animal=cat";
+        //Creating an AJax call for the specific pet button being click
+        $.ajax({
+            url: queryURL,
+
+            method: "GET"
+        }).then(function (fetch) {
+            var results = fetch;
+            console.log(results);
+            var innerResult = results.petfinder.pets.pet;
+            // console.log(results)
             
             for (let i = 0; i < innerResult.length; i++) {
                 const element = innerResult[i];
@@ -199,35 +381,3 @@ function breed() {
 //have to do that for breed...done gender age distance and size.
 
 //create a function
-
-var petBreeds =["Retrievers"
-    "German Shepherd",
-    "Retrievers",
-    "French Bulldogs",
-    "Bulldogs",
-    "Beagles",
-    "Poodles",
-    "Rottweilers",
-    "Yorkshire Terriers",
-    "Boxers",
-    "Huskies",
-    "Dachshunds",
-    "Great Danes",
-    "Doberman Pinschers
-    "Miniature Schnauzers
-    "Shih Tzu
-    "Cavalier King Charles Spaniels
-    "Boston Terriers
-    Pomeranians
-    Havanese
-    Shetland Sheepdogs
-    Bernese Mountain Dogs
-    Brittanys
-    Spaniels
-    Mastiffs"]
-
-    function breeds() {
-
-
-        
-    }
